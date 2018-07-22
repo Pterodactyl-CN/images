704da5ebd9e93db7aa29ab11a455d12269e72900
gzzchh <xjdzch@126.com>
2018-07-22 21:31:45 +0800

    Show Java Version

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
+java -version
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
