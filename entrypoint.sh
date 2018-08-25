1c13c1ee3d42019e7016bbd1148f96b9c75e78c7
gzzchh <xjdzch@126.com>
2018-08-26 00:05:56 +0800

    Fix entrypointsh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
-java -version
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
