e0eea0cc499b082e571e350b7d39afa89e748acf
gzzchh <xjdzch@126.com>
2018-08-29 13:18:12 +0800

    no message

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,6 @@ java -version
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
-echo "版权所有：MC部落 御坂云 TrollCraft SakuraMC 阿卡林云 零梦云 云上聊城，保留所有权利。"
-echo "Powered By:MClans MisakaCloud TrollCraft SakuraMC AkkariinCloud ZeroDream CloudLiaocheng,All Rights Reserved."
+curl https://raw.githubusercontent.com/Pterodactyl-CN/images/master/ad.txt
 # Run the Server
 eval ${MODIFIED_STARTUP}
