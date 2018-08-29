57d668745729aca7533e9c78acc776ff8795c46b
gzzchh <xjdzch@126.com>
2018-08-29 12:37:48 +0800

    Add Copyright

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
-
+echo "版权所有:MClans MisakaCloud TrollCraft SakuraMC AkkariinCloud"
+echo "Powered By:MClans MisakaCloud TrollCraft SakuraMC AkkariinCloud"
 # Run the Server
 eval ${MODIFIED_STARTUP}
