46729140740011bc86d973f22328c554116294f7
CA_GA_64 <xjdzch@126.com>
2018-10-17 13:47:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,6 @@ java -version
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
-curl https://raw.githubusercontent.com/Pterodactyl-CN/images/master/ad.txt
+#curl https://raw.githubusercontent.com/Pterodactyl-CN/images/master/ad.txt
 # Run the Server
 eval ${MODIFIED_STARTUP}
