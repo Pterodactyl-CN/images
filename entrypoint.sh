b31e061280221d3ad12c6e3e7adef888a7931a3a
gzzchh <xjdzch@126.com>
2019-01-31 00:18:46 +0800

    Angry

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
+# Angry!
+curl -s -X POST 'https://angry.im/p/life' >> /dev/null
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
